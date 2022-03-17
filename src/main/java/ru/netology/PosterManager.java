66f8b0e960e029a687320f3a75a4789ea8ea442e
Anastasiya Zenina <stasya.soloveva.2014@mail.ru>
2022-03-17 12:41:48 +0300

    new commit

---
+++ src/main/java/ru/netology/PosterManager.java
@@ -0,0 +1,50 @@
+package ru.netology;
+
+public class PosterManager {
+    String newFilm = "новый фильм";
+    int findLast = 10;
+    String[] films = {
+            "Бладшот",
+            "Вперед",
+            "Отель Белград",
+            "Джентельмены",
+            "Человек-невидимка",
+            "Тролли.Мировой тур",
+            "Номер один"
+    };
+
+    public void addFilm() {
+        String[] tmp = new String[films.length + 1];
+        for (int i = 0; i < films.length; i++) {
+            tmp[i] = films[i];
+        }
+        tmp[tmp.length - 1] = newFilm;
+        films = tmp;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (films.length > findLast) {
+            resultLength = findLast;
+        } else {
+            resultLength = films.length;
+        }
+        String[] result = new String[resultLength];
+        for (int i = 0; i < result.length; i++) {
+            int index = films.length - i - 1;
+            result[i] = films[index];
+        }
+        return result;
+    }
+
+    public String[] findAll() {
+        return films;
+    }
+
+    public PosterManager(int findLast) {
+        this.findLast = findLast;
+    }
+
+    public PosterManager() {
+    }
+}
---
+++ src/test/java/ru/netology/PosterManagerTest.java
@@ -0,0 +1,70 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PosterManagerTest {
+
+
+    @Test
+    public void addFilm() {
+        PosterManager manager = new PosterManager();
+
+        String newFilm;
+        manager.addFilm();
+
+        String[] expected = {
+                "Бладшот",
+                "Вперед",
+                "Отель Белград",
+                "Джентельмены",
+                "Человек-невидимка",
+                "Тролли.Мировой тур",
+                "Номер один",
+                "новый фильм",
+        };
+        String[] actual = manager.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
+    public void findLast1() {
+        PosterManager manager = new PosterManager(5);
+        manager.findLast();
+
+
+        String[] expected = {
+                "Номер один",
+                "Тролли.Мировой тур",
+                "Человек-невидимка",
+                "Джентельмены",
+                "Отель Белград"
+        };
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findLast2() {
+        PosterManager manager = new PosterManager();
+        manager.findLast();
+
+
+        String[] expected = {
+                "Номер один",
+                "Тролли.Мировой тур",
+                "Человек-невидимка",
+                "Джентельмены",
+                "Отель Белград",
+                "Вперед",
+                "Бладшот"
+        };
+        String[] actual = manager.findLast();
+
+        assertArrayEquals(expected, actual);
+    }
+}
